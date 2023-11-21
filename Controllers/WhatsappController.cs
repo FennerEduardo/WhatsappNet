3c5ebe691ab0ec2ff522306e92b36a60625ca2e6
Fenner Eduardo González Castellanos <fennereduardo@gmail.com>
2023-11-21 13:02:56 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/WhatsappController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WhatsappNet.Controllers
+{
+    [ApiController]
+    [Route("api/whatsapp")]
+    public class WhatsappController : Controller
+    {
+        [HttpGet]
+        public IActionResult Sample()
+        {
+            return Ok("Ok sample");
+        }
+    }
+}
